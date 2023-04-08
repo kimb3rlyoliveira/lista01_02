f30b1b0c6eb32f1dca625ae3aceada0194c9b526
kiimolliveiraa <kiimolliveiraa@gmail.com>
2023-04-07 23:48:52 -0300

    Primeiro commit

---
+++ src/App.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class App {
+  
+  public static void main(String[] args) {
+    double numero;
+
+    System.out.print("Insira um número: ");
+    try (
+    Scanner teclado = new Scanner(System.in)) {
+      numero = teclado.nextDouble();
+    teclado.close();
+    }
+
+    System.out.println("O número informado foi: " + numero);
+  }
+}
